e62982ca9184ca0105a86353ad309169cb6d19c0
ilieBeracha <ilieberacha123@gmail.com>
2022-12-17 15:35:41 +0200

    navbar

--- src/Components/Main/Content/Content.tsx
+++ src/Components/Main/Content/Content.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./Content.css";
 
 function Content(): JSX.Element {
     return (
         <div className="Content">
-			
+			<Routes>
+                <Route path="/home" element={'home'}></Route>
+                <Route path="/library" element={'library'}></Route>
+                <Route path="/playlists" element={'playlists'}></Route>
+            </Routes>
         </div>
     );
 }
--- src/Components/Main/Header/Navbar.tsx
+++ src/Components/Main/Header/Navbar.tsx
@@ -1,9 +1,22 @@
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar(): JSX.Element {
     return (
         <div className="Navbar">
-			
+            <nav className="side-bar">
+                <ul>
+                    <li className="active">
+                        <NavLink to={'/home'}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/library'}>Library</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/playlists'}>Playlists</NavLink>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
